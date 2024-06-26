cbbd5b61f49f3e0a4ba5958c8cdc8fab4447b184
Ryan Rodrigues <ryan.rodrigues62@hotmail.com>
2024-06-26 17:42:20 -0300

    feat: paginação completa

--- src/Pages/Result/Result.jsx
+++ src/Pages/Result/Result.jsx
@@ -3,20 +3,64 @@ import SearchContent from "../../Components/SearchContent";
 import styles from "./Result.module.css";
 import ResultItem from "../../Components/ResultItem";
 import { SearchContext } from "../../SearchContext";
+import {
+    ChevronLeft,
+    ChevronsLeft,
+    ChevronRight,
+    ChevronsRight,
+} from "lucide-react";
 
 function Result() {
-  const {search, setSearch, data} = React.useContext(SearchContext);
+    const { search, setSearch, data, setPage, page } =
+        React.useContext(SearchContext);
+
+    const total = Math.ceil(data.totalHits / 10);
+
+    function goToFirstPage(){
+        setPage(1);
+    }
+
+    function goToNextPage() {
+            setPage((oldPage) => oldPage + 1);
+    }
+
+    function goToPreviousPage() {
+            setPage((oldPage) => oldPage - 1);
+       
+    }
+
+    function goToLastPage() {
+        setPage(total);
+    }
+    console.log(page)
     return (
         <div className="container">
-            <SearchContent search={search} setSearch={setSearch}/>
+            <SearchContent search={search} setSearch={setSearch} />
             <div className={styles.info}>
                 <p>Resultados Encontrados</p>
-                <p className={styles.resultados}>{data.length} resultados</p>
+                <p className={styles.resultados}>{data.totalHits} resultados</p>
             </div>
             <div className={styles.resultadosContainer}>
-                {data.map((item, index) => (
-                    <ResultItem key={index} data={item}/>
+                {data.Results.map((item, index) => (
+                    <ResultItem key={index} data={item} />
                 ))}
+            </div>
+            <div className={styles.paginacao}>
+                <p>Página {page} de {total}</p>
+                <div className={styles.icons}>
+                    <div onClick={goToFirstPage} className={page === 1 ? styles.disabled : ''}>
+                        <ChevronsLeft color="#fff" />
+                    </div>
+                    <div onClick={goToPreviousPage} className={page === 1 ? styles.disabled : ''}>
+                        <ChevronLeft color="#fff"  />
+                    </div>
+                    <div onClick={goToNextPage} className={page === total ? styles.disabled : ''}>
+                        <ChevronRight color="#fff" />
+                    </div>
+                    <div onClick={goToLastPage} className={page === total ? styles.disabled : ''}>
+                        <ChevronsRight color="#fff" />
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/SearchContext.jsx
+++ src/SearchContext.jsx
@@ -1,28 +1,79 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export const SearchContext = React.createContext();
 
 export const SearchProvider = ({ children }) => {
+    /* const [search, setSearch] = React.useState(() => {
+        const url = new URL(window.location.toString());
+        const search = url.searchParams.get("query");
+        return search ? search : "";
+    }); */
     const [search, setSearch] = React.useState("");
+    const [page, setPage] = React.useState(1);
+    /* const [page, setPage] = React.useState(() => {
+        const url = new URL(window.location.toString());
+        const page = url.searchParams.get("page");
+        return page ? Number(page) : 1;
+    }); */
+
     const navigate = useNavigate();
+    const location = useLocation();
     const [data, setData] = React.useState([]);
-    async function handleSubmit(event) {
-        event.preventDefault();
+
+    /* const updateURLParams = (search, page) => {
+        const url = new URL(window.location.toString());
+        url.searchParams.set("query", search);
+        url.searchParams.set("page", page);
+        window.history.pushState({}, "", url);
+    }; */
+
+    const fetchData = async (search, page) => {
         const response = await fetch(
-            `http://localhost:8080/v1/search?query=${search}?page=3`
+            `http://localhost:8080/v1/search?query=${search}&page=${page}`
         );
         const json = await response.json();
-        if(response.ok) {
-          setData(json);
-          navigate("/result");
+        if (response.ok) {
+            setData(json);
+            
         }
-        console.log(data);
         console.log(json);
-    }
+    };
+
+    const handleSubmit = async (event) => {
+        if (event) event.preventDefault();
+        if (search.length < 1) return;
+        await fetchData(search, page);
+        
+        navigate("/result");
+        //updateURLParams(search, page);
+    };
+
+    React.useEffect(() => {
+        fetchData(search, page);
+    }, [page])
+
+    /* React.useEffect(() => {
+        const url = new URL(window.location.toString());
+        const query = url.searchParams.get("query");
+        const pageNum = url.searchParams.get("page");
+
+        if (query) {
+            setSearch(query);
+        }
+        if (pageNum) {
+            setPage(Number(pageNum));
+        }
+
+        if (query) {
+            fetchData(query, pageNum ? Number(pageNum) : 1);
+        }
+    }, [location.search]); */
+
+
     return (
         <SearchContext.Provider
-            value={{ search, setSearch, handleSubmit, data }}
+            value={{ search, setSearch, handleSubmit, data, setPage, page }}
         >
             {children}
         </SearchContext.Provider>
